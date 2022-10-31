2285789128a3a3744f7b5dd0696635a2ed35be37
William Hirsch <Williamhirschh@gmail.com>
2022-10-31 14:44:26 -0500

    made the html and is set up to display the input information,

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ inquirer
       {
         type: 'input',
         message: 'What are the contribution guidelines?',
-        name: 'contribution guidelines',
+        name: 'contribution',
       },
       {
         type: 'input',
         message: 'What are the test instructions?',
-        name: 'test instructions',
+        name: 'test',
 
       },
       {
@@ -45,3 +45,27 @@ inquirer
         name: 'email',
       },
   ])
+  .then((answers) => {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>README</title>
+</head>
+<body>
+    <header class="header">${answers.name}</header>
+    <p class="description">${answers.description}</p>
+    <h1 class="Installation">Installation</h1>
+    <p class="description">${answers.installation}</p>
+    <h2 class="usage">Usage</h2>
+    <p class="description">${answers.usage}</p>
+    <h3 class="contribution">Contribution Guidelines</h3>
+    <p class="description">${answers.contribution}</p>
+</body>
+</html>`
+fs.writeFile('answers.html', html, (err) =>
+err ? console.error(err) : console.log('Success!')
+);
+  });

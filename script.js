ea3525cb9c2ceae02d074511c6074588788bf3c4
William Hirsch <Williamhirschh@gmail.com>
2022-11-01 12:38:05 -0500

    filled out the rest of html and included images and styling

--- script.js
+++ script.js
@@ -44,6 +44,11 @@ inquirer
         message: 'What is your email adress?',
         name: 'email',
       },
+      {
+        type: 'input',
+        message: 'What license are you using?',
+        name: 'license',
+      },
   ])
   .then((answers) => {
     const html = `<!DOCTYPE html>
@@ -52,17 +57,35 @@ inquirer
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>README</title>
 </head>
 <body>
     <header class="header">${answers.name}</header>
+    <header class="Descript">Description</header>
     <p class="description">${answers.description}</p>
+    <header class="tableofcontents">Table of Contents </header>
+    <div class="quicklinks">
+    <a href=".Installation" class="links">Installation</a>
+    <a href=".usage" class="links">Usage</a>
+    <a href=".contribution" class="links">Contribution Guidelines</a>
+    <a href=".test" class="links">Test Instructions</a>
+    <a href=".questions" class="links">Questions</a>
+    <a href=".license" class="links">License</a>
+    </div>
     <h1 class="Installation">Installation</h1>
     <p class="description">${answers.installation}</p>
     <h2 class="usage">Usage</h2>
     <p class="description">${answers.usage}</p>
     <h3 class="contribution">Contribution Guidelines</h3>
     <p class="description">${answers.contribution}</p>
+    <h4 class="test">Test Instructions</h4>
+    <p class="description">${answers.test}</p>
+    <h5 class="questions">Questions</h5>
+    <p class="description">${answers.GitHub, answers.email} <br>WilliamHirschh</p>
+    <h6 class="license">license</h6>
+    <p class="LicenseFiller">${answers.license}</p>
+    <img class="img" src="MIT.png">
 </body>
 </html>`
 fs.writeFile('answers.html', html, (err) =>
